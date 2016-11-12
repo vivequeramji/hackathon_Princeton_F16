f3e5a301cf1053290499f3236d2f7172cad4ccf3
melanahammel <mhammel216@gmail.com>
2016-11-12 11:51:20 -0500

    Add files via upload

--- building_dir_sym_tabl.py
+++ building_dir_sym_tabl.py
@@ -12,35 +12,17 @@ class Point(object):
 		test = str(self.x) + " " + str(self.y)
 		print test 
 
-#testPoint = Point(1,2)
-#testPoint.print_point()
-
-'''
-def make_point(x, y)
-	point = Point(x,y)
-	return point
-'''
-'''
-testPoint = Point(1,2)
-test = str(testPoint.x) + " " + str(testPoint.y)
-print test 
-'''
-
-with open('buildingDirTest.csv', 'rb') as f:
+with open('locations.csv', 'rb') as f:
+	
 	reader = csv.reader(f)
-
 	building_names = {}
-
-	next(f)
+	
 	for row in reader:
 		point = Point(row[1], row[2])
-		point.print_point()
-		#name = row[0]
 		building_names[row[0]] = point 
-		#school_districts.append(row[3])
-		#school_addresses.append(row[10] + " " + row[12])
 	f.close()
-
+'''
 for key,value in building_names.items():
-    print key #+ " => " + value
+    print key 
     value.print_point()
+'''
