8889f224bfc451816b665699f4d2358d4f3d417e
Viveque Ramji <viveque.ramji@gmail.com>
2016-11-12 12:53:26 -0500

    Update email_info.py

--- email_info.py
+++ email_info.py
@@ -42,3 +42,42 @@ for part in msg.walk():
 print subject
 
 print body[2]
+
+
+
+
+
+
+
+
+
+
+Viveque:
+    
+    
+    import numpy as np
+import matplotlib.pyplot as plt
+# from building_dir_sym_tabl import building_names
+from PIL import Image
+
+
+def plot(place, time):
+	size = time.time - time
+	alp = 0.5 + (size/7200.0)
+	plt.scatter(x=place.x, y=place.y, alpha=alp)
+
+
+
+fname = 'map.jpg'
+image = Image.open(fname)
+arr = np.asarray(image)
+plt.imshow(arr)
+
+
+p
+# plt.scatter([1500], [1500], alpha=0.5)
+
+# put a red dot, size 40, at 2 locations:
+
+
+plt.show()
